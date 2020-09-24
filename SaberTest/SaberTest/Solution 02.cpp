76d1ba440fb62a6b6aa219dbc4ea4f3ca3878f65
GArGuTZ <gargutz@gmail.com>
2020-09-24 12:50:45 +0300

    Added Solution 02

---
+++ SaberTest/SaberTest/Solution 02.cpp
@@ -0,0 +1,46 @@
+#include "Solution 02.h"
+
+void RemoveDups(char* _str)
+{
+	int inputLength = strlen(_str);
+
+	if (inputLength < 2) // Primitive case
+	{
+		return;
+	}
+
+	// Count length of new str
+	char currentSymbol = _str[0];
+	int newLength = 2;
+	for (int i = 1; i < inputLength; ++i)
+	{
+		if (_str[i] != currentSymbol)
+		{
+			currentSymbol = _str[i];
+			++newLength;
+		}
+	}
+
+	char* newStr = new char[newLength];
+	newStr[newLength - 1] = '\0';
+
+	// Delete duplicates
+	currentSymbol = _str[0];
+	newStr[0] = _str[0];
+	int j = 1; // Counter for new str
+	for (int i = 1; i < inputLength; ++i)
+	{
+		if (_str[i] != currentSymbol)
+		{
+			currentSymbol = _str[i];
+			newStr[j] = _str[i];
+			++j;
+		}
+	}
+
+	strcpy(_str, newStr);
+
+	delete [] newStr;
+
+	return;
+}
---
+++ SaberTest/SaberTest/Solution 02.h
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void RemoveDups(char* _str);
